59677e8b16303fd2641d2b1944c20e2f2eb3a3b2
Simon Larsson <99037566+simlar-0@users.noreply.github.com>
2025-05-28 14:57:28 +0200

    Add hooks to RISCV

--- symex/src/arch/risc_v/mod.rs
+++ symex/src/arch/risc_v/mod.rs
@@ -2,16 +2,22 @@
 
 use std::fmt::Display;
 
+use anyhow::Context;
+
 use crate::{
     arch::{ArchError, Architecture, ArchitectureOverride, SupportedArchitecture, InterfaceRegister},
     executor::{
-        hooks::HookContainer,
+        hooks::{HookContainer, PCHook},
         instruction::Instruction,
         state::GAState,
     },
+    smt::{SmtExpr, SmtMap},
+    trace,
+    debug,
     project::dwarf_helper::SubProgramMap,
     Composition,
 };
+
 
 pub mod decoder;
 pub mod decoder_implementations;
@@ -27,29 +33,76 @@ impl<Override: ArchitectureOverride> Architecture<Override> for RISCV {
         unimplemented!();
     }
 
-    fn add_hooks<C: Composition>(&self, hooks: &mut HookContainer<C>, sub_program_lookup: &mut SubProgramMap) {
-        unimplemented!();
+    fn add_hooks<C: crate::Composition>(&self, cfg: &mut HookContainer<C>, map: &mut SubProgramMap) {
+        trace!("Adding RISCV hooks");
+        let symbolic_sized = |state: &mut GAState<C>| {
+            let value_ptr = match state.memory.get_register("X10") {
+                Ok(val) => val,
+                Err(e) => return Err(e).context("While resolving address for new symbolic value"),
+            };
+
+            let size = (match state.memory.get_register("X11") {
+                Ok(val) => val,
+                Err(e) => return Err(e).context("While resolving size for new symbolic value"),
+            })
+            .get_constant()
+            .unwrap()
+                * 8;
+            let name = state.label_new_symbolic("any");
+            if size == 0 {
+                let register_name = state.architecture.get_register_name(InterfaceRegister::ReturnAddress);
+                let ra = state.get_register(register_name.to_owned()).unwrap();
+                state.set_register("PC".to_owned(), ra)?;
+                return Ok(());
+            }
+            let symb_value = state.memory.unconstrained(&name, size as u32);
+            // We should be able to do this now!
+            // TODO: We need to label this with proper variable names if possible.
+            //state.marked_symbolic.push(Variable {
+            //    name: Some(name),
+            //    value: symb_value.clone(),
+            //    ty: ExpressionType::Integer(size as usize),
+            //});
+
+            match state.memory.set(&value_ptr, symb_value) {
+                Ok(_) => {}
+                Err(e) => return Err(e).context("While assigning new symbolic value"),
+            };
+
+            let register_name = state.architecture.get_register_name(InterfaceRegister::ReturnAddress);
+            let ra = state.get_register(register_name.to_owned()).unwrap();
+            state.set_register("PC".to_owned(), ra)?;
+            Ok(())
+        };
+
+        if let Err(_) = cfg.add_pc_hook_regex(map, r"^symbolic_size$", PCHook::Intrinsic(symbolic_sized)) {
+            debug!("Could not add symoblic hook, must not contain any calls to `symbolic_size`");
+        }
+        if let Err(_) = cfg.add_pc_hook_regex(map, r"^symbolic_size<.+>$", PCHook::Intrinsic(symbolic_sized)) {
+            debug!("Could not add symoblic hook, must not contain any calls to `symbolic_size<.+>`");
+        }
+
+        if let Err(_) = cfg.add_pc_hook_regex(map, r"^HardFault.*$", PCHook::EndFailure("Hardfault")) {
+            trace!("Could not add hardfault hook");
+        }
     }
 
     fn pre_instruction_loading_hook<C>(state: &mut GAState<C>)
     where
         C: Composition<ArchitectureOverride = Override>,
     {
-        unimplemented!();
     }
 
     fn post_instruction_execution_hook<C>(state: &mut GAState<C>)
     where
         C: Composition<ArchitectureOverride = Override>,
     {
-        unimplemented!();
     }
 
     fn initiate_state<C>(state: &mut GAState<C>)
     where
         C: Composition<ArchitectureOverride = Override>,
     {
-        unimplemented!();
     }
 
     fn get_register_name(reg:InterfaceRegister) -> String {
